e588ef52300252f0e79b72ebfe35e996c1c4041d
svetk0 <sv.solomatnikova@gmail.com>
2023-12-26 21:38:36 +0300

    change title and icon in the Tab

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './components/Header';
+import { useEffect }  from 'react';
 import HeroCards from './components/HeroCards';
 import "./components/HeroesCards.css";
 import { data } from './components/Data-main';
 
 function App() {
+  useEffect(() => { 
+    document.title = "Heroes Cards";
+  }, []);
+  
   return (
     <div className="container-main">
        <Header />
